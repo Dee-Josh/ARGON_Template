d435c3e4ebe24e2b451099ea38aacd6fb2bde094
Joshua Dada <joshwebprog@gmail.com>
2025-08-30 23:45:22 +0100

    Updated

--- screens/Authentication/SignIn.jsx
+++ screens/Authentication/SignIn.jsx
@@ -1,3 +1,15 @@
 import React, {useState, useEffect, useContext} from 'react';
 import {View, ImageBackground, TouchableOpacity} from 'react-native';
+import { Text, TextInput } from ''
 
+export default  function SignInScreen () {
+
+    return (
+        <>
+            <View>
+                <Text>Welcome Back</Text>
+                <TextInput />
+            </View>
+        </>
+    )
+}
