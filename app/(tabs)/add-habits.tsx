c69d223ea781aa9e3264b05da7155814d15dbe89
Joshua Dada <joshwebprog@gmail.com>
2025-09-02 20:55:32 +0100

    Updated

--- app/(tabs)/add-habits.tsx
+++ app/(tabs)/add-habits.tsx
@@ -1,19 +1,36 @@
-import { Text, View } from "react-native";
-import { TextInput } from "react-native-paper";
+import { Text, View, StyleSheet } from "react-native";
+import { SegmentedButtons, TextInput, Button } from "react-native-paper";
 
+const FREQUENCIES = ["daily", "weekly", "monthly"]
 
 
 export default function AddHabitsScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
         label="Title"
         mode="outlined"
+        style={styles.input}
       />
       <TextInput
         label="Description"
         mode="outlined"
+        style={styles.input}
       />
+      <View>
+        <SegmentedButtons buttons={FREQUENCIES.map((freq)=> ({
+          value: freq,
+          label: freq.charAt(0).toUpperCase() + freq.slice(1)
+        })} />
+      </View>
+      <Button mode="contained">
+        Add Habit
+      </Button>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+
+})
