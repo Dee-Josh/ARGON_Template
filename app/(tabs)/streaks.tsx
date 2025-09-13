e4b60d1e4816e250514fd1e12b76456d9454dca0
Joshua Dada <joshwebprog@gmail.com>
2025-09-13 18:55:37 +0100

    Streak Screen Coding

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -4,14 +4,22 @@ import { Text, TextInput } from "react-native-paper";
 
 
 export default function StreaksScreen() {
+
+  const habits = [0, 1, 2, 3];
+
+
   return (
     <View
       style={styles.container}
     >
       <Text>Habits Streak</Text>
+      {
+      habits.length === 0 ? (
+      <View style={styles.emptyState}>
+        <Text style={styles.emptyStateText}> No Habits Yet. Add your first Habit!</Text>
+      </View>) : ()
+    }
     </View>
-    
-
   );
 }
 
