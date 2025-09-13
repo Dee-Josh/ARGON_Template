17073f39357602f00cec94166290d2fa282ba626
Joshua Dada <joshwebprog@gmail.com>
2025-09-13 19:25:20 +0100

    Streak Screen Coding

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,12 +8,68 @@ import { Surface, Text } from "react-native-paper";
 import Toast from "react-native-toast-message";
 
 
-
+//   const today = new Date().toDateString();
+//   export const [habits, setHabits] = useState([
+//   {
+//     title: "Do this Once",
+//     description: "Drink 25 million gallons of water.",
+//     streak_count: 0,
+//     frequency: "Daily",
+//     date_completed: "",
+//     id: 0,
+//     idd: "hfffhh",
+//   },
+//   {
+//     title: "Do this Twice",
+//     description: "Drink 25 million gallons of water.",
+//     streak_count: 1,
+//     frequency: "Daily",
+//     // date_completed: "",
+//     date_completed: today,
+//     id: 1,
+//     idd: "hfhfhfh",
+//   },
+//   {
+//     title: "Do this Thrice",
+//     description: "Drink 25 million gallons of water.",
+//     streak_count: 0,
+//     frequency: "Daily",
+//     date_completed: "",
+//     id: 2,
+//     idd: "hfhfhfffh",
+//   },
+//   {
+//     title: "Do this Four-ice",
+//     description: "Drink 25 million gallons of water.",
+//     streak_count: 0,
+//     frequency: "Daily",
+//     date_completed: "",
+//     id: 3,
+//     idd: "hfhfhfjfjh",
+//   },
+//   {
+//     title: "Do this Five-ice",
+//     description: "Drink 25 million gallons of water.",
+//     streak_count: 0,
+//     frequency: "Daily",
+//     date_completed: "",
+//     id: 4,
+//     idd: "hfhfhjkkffh",
+//   },
+//   {
+//     title: "Do this Six-ice",
+//     description: "Drink 25 million gallons of water.",
+//     streak_count: 0,
+//     frequency: "Daily",
+//     date_completed: "",
+//     id: 5,
+//     idd: "kfkf",
+//   },
+// ])
 
 
 export default function Index() {
   const today = new Date().toDateString();
-
   const [habits, setHabits] = useState([
   {
     title: "Do this Once",
@@ -71,6 +127,7 @@ export default function Index() {
     idd: "kfkf",
   },
 ])
+
 
   const [isSwiped, setIsSwiped] = useState('');
   const [completedHabits, setCompletedHabits] = useState();
